e344d6f31079e32ba60da1bc09b3abf12b6bc122
khoazmi <khoazmi@gmail.com>
2024-09-18 13:42:46 +0700

    menyelesaikan tutorial 3 menambhakan fungsi sign in, login, log out user

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1+v(b^-8_0+_-k6p88)&4ybyf(-%#p4i7%cy#4v1__*b^y2x2!'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "khoirul-azmi-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
